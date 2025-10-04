c6e16a0651462499ae05a2f60a465ed84d92cd64
Sefin Saju <sefinsaju777@gmail.com>
2025-10-04 18:15:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector(".intro-btn").addEventListener("click",()=>{
+    document.querySelector(".intro-btn").classList.add("fadeout");
+    document.querySelectorAll(".x").forEach(el => {
+        el.classList.add("fadeout");
+    });
+    document.querySelector(".intro-info1").classList.add("fadein");
+    document.querySelectorAll(".y").forEach(el=>{
+        el.classList.add("fadein");
+    })
+});
+document.querySelectorAll(".scroll-btn").forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        const currentContainer=btn.closest(".story-container");
+        const nextContainer=currentContainer.nextElementSibling;
+        nextContainer.scrollIntoView({behavior:"smooth",inline:"start"});
+    })
+});
+
+document.querySelectorAll(".scroll-back").forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        const currentContainer1=btn.closest(".story-container");
+        const prevContainer=currentContainer1.previousElementSibling;
+        prevContainer.scrollIntoView({behavior:"smooth",inline:"start"});
+    })
+});
+
+
